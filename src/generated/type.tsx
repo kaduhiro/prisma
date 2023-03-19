38a9f80cd74afda1dc0c46e1a86986d0019e1ca1
kaduhiro <github@kaduhiro.com>
2023-03-19 21:48:56 +0900

    refactor: add types for prisma data conversion

---
+++ src/generated/type.tsx
@@ -0,0 +1,29 @@
+import { Post } from '@prisma/client';
+
+// read
+export type RequestPostsData = {
+  limit: number;
+};
+
+export type ResponsePostsData = {
+  posts: Post[];
+  count: number;
+};
+
+// create
+export type RequestCreatePostData = {
+  text: string;
+};
+
+export type ResponseCreatePostData = {
+  post: Post;
+};
+
+// delete
+export type RequestDeletePostData = {
+  id: number;
+};
+
+export type ResponseDeletePostData = {
+  post: Post;
+};
