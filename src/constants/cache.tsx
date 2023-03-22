096793eded707ba4b6daac4e14142c1fd156c950
kaduhiro <github@kaduhiro.com>
2023-03-22 20:26:42 +0900

    feat: add constants for cache

---
+++ src/constants/cache.tsx
@@ -0,0 +1,12 @@
+export const _RequestType = {
+  create: 'create',
+  read: 'read',
+  update: 'update',
+  delete: 'delete',
+} as const;
+
+export const _Post = {
+  cacheKey: {
+    post: 'post',
+  },
+} as const;
