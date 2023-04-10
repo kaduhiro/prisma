12c4c5e41aaf608fee496f188931841a13eb77b8
kaduhiro <github@kaduhiro.com>
2023-04-10 20:31:59 +0900

    feat: add types for generic data access API

---
+++ src/types/_.tsx
@@ -0,0 +1,83 @@
+// repository
+export type RequestListData = {
+  limit: number;
+};
+
+export type ResponseListData<T> = {
+  data: T[];
+  count: number;
+};
+
+export type RequestReadData = {
+  id: number | string;
+};
+
+export type ResponseReadData<T> = {
+  data: T;
+};
+
+export type RequestCreateData = Partial<{
+  [key: string]: number | string | Date;
+}>;
+
+export type ResponseCreateData<T> = {
+  data: T;
+};
+
+export type RequestUpdateData = Partial<{
+  [key: string]: number | string | Date;
+}>;
+
+export type ResponseUpdateData<T> = {
+  data: T;
+};
+
+export type RequestDeleteData = {
+  id: number | string;
+};
+
+export type ResponseDeleteData<T> = {
+  data: T;
+};
+
+// usecase
+export type ListQuery = {
+  limit?: number;
+};
+
+export type ListResponse<T> = {
+  data: T[];
+  count: number;
+};
+
+export type ReadQuery = {
+  id: number | string;
+};
+
+export type ReadResponse<T> = {
+  data: T;
+};
+
+export type CreateQuery = Partial<{
+  [key: string]: number | string | Date;
+}>;
+
+export type CreateResponse<T> = {
+  data: T;
+};
+
+export type UpdateQuery = Partial<{
+  [key: string]: number | string | Date;
+}>;
+
+export type UpdateResponse<T> = {
+  data: T;
+};
+
+export type DeleteQuery = {
+  id: number | string;
+};
+
+export type DeleteResponse<T> = {
+  data: T;
+};
--- src/types/index.tsx
+++ src/types/index.tsx
@@ -1,2 +1,3 @@
+export * from './_';
 export * from './api-client';
 export * from './layout';
