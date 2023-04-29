4c4c0efa9677e0491b2186f59748d584e3c95dd1
kaduhiro <github@kaduhiro.com>
2023-04-29 20:15:13 +0900

    refactor: change mutate process

--- src/hooks/useMutate.tsx
+++ src/hooks/useMutate.tsx
@@ -5,11 +5,16 @@ export const useMutate = () => {
   const { cache, mutate } = useSWRConfig();
 
   const clearMutate = (key: Arguments, data?: any, opts?: boolean | MutatorOptions<any> | undefined) => {
+    if (Array.isArray(key)) {
+      key = key.filter((v) => v);
+    }
+
     const serialized = serialize(key);
-    Array.from<string>(cache.keys()).forEach((value) => {
-      if (value.replace(/,$/, '').indexOf(serialized[0].replace(/,$/, '')) === 0) {
-        mutate(value, data, opts);
-      }
+
+    const keys = Array.from<string>(cache.keys()).filter((key) => key.startsWith(serialized[0]));
+
+    keys.map((key) => {
+      mutate(key, undefined, true);
     });
   };
 
